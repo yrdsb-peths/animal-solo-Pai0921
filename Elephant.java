cb8574884ad518b7dbfd209be86c0a06e22b979b
Pai0921 <440042698@gapps.yrdsb.ca>
2025-04-28 13:03:25 -0400

    Part1

---
+++ Elephant.java
@@ -0,0 +1,42 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The Elephant, our hero.
+ * 
+ * @author Pai
+ * @version 2025
+ */
+public class Elephant extends Actor
+{
+    /**
+     * Act - do whatever the Elephant wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        if(Greenfoot.isKeyDown("left"))
+        {
+            move(-1);
+        }
+        else if(Greenfoot.isKeyDown("right"))
+        {
+            move(1);
+        }
+        
+        //Remove allpe if elephant eats it
+        eat();
+    }
+    
+    public void eat()
+    {
+        
+        if(isTouching(Apple.class))
+        {
+            removeTouching(Apple.class);
+            MyWorld world = (MyWorld) getWorld();
+            world.createApple();
+            world.increaseScore();
+        }
+    }
+}
